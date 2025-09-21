63abde05c2b8eb9e453ad7d66daff2a4e503c5c4
Jan Altrichter <honzaaltrichter@gmail.com>
2025-09-21 12:22:12 +0300

    Retries connecting to websocket on fail

--- WebsocketClient.js
+++ WebsocketClient.js
@@ -3,7 +3,25 @@ function greet(name) {
 }
 var message = greet("World");
 console.log(message);
-var socket = new WebSocket("ws://localhost:4242");
-socket.addEventListener("message", function (event) {
-    console.log("Received:", event.data);
-});
+function connect() {
+    var socket = new WebSocket("ws://localhost:4242");
+    socket.onopen = function () {
+        console.log("Connection opened");
+    };
+    socket.onmessage = function (event) {
+        console.log("Received:", event.data);
+    };
+    socket.onclose = function () {
+        console.log("Connection closed");
+        setTimeout(connect, 5000);
+    };
+    socket.onerror = function (error) {
+        console.error("WebSocket error:", error);
+        socket.close(); // ensure it triggers onclose and retries
+    };
+}
+connect();
+// socket.addEventListener("message", (event) => {
+//     console.log("Received:", event.data);
+// });
+/// P2PCF?
--- WebsocketClient.ts
+++ WebsocketClient.ts
@@ -5,7 +5,32 @@ function greet(name: string): string {
 const message: string = greet("World");
 console.log(message);
 
-let socket = new WebSocket("ws://localhost:4242");
-socket.addEventListener("message", (event) => {
-    console.log("Received:", event.data);
-});
+function connect(){
+    let socket = new WebSocket("ws://localhost:4242");
+
+    socket.onopen = () => {
+        console.log("Connection opened");
+    }
+
+    socket.onmessage = (event: { data: any; }) => {
+        console.log("Received:", event.data);
+    }
+
+    socket.onclose = () => {
+        console.log("Connection closed");
+        setTimeout(connect, 5000);
+    }
+
+    socket.onerror = (error: any) => {
+        console.error("WebSocket error:", error);
+        socket.close(); // ensure it triggers onclose and retries
+    };
+}
+
+
+connect();
+// socket.addEventListener("message", (event) => {
+//     console.log("Received:", event.data);
+// });
+
+/// P2PCF?
