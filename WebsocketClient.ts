5ec82de72f3035eeda170d29cbe722a2ae034423
Jan Altrichter <xaltrich@fi.muni.cz>
2025-09-22 13:34:12 +0200

    add timeout

--- WebsocketClient.ts
+++ WebsocketClient.ts
@@ -1,35 +1,34 @@
 
-function startup() {
-    let urlParams = new URLSearchParams(window.location.search);
+setTimeout(() => {}, 1000);
+let urlParams = new URLSearchParams(window.location.search);
 
-    document.getElementById("test1").innerHTML = urlParams.get("username") + " is trying to connect to room associated with server " + urlParams.get("server_id");
+document.getElementById("test1").innerHTML = urlParams.get("username") + " is trying to connect to room associated with server " + urlParams.get("server_id");
 
-    function connect() {
-        let socket = new WebSocket("ws://localhost:4242");
+function connect() {
+    let socket = new WebSocket("ws://localhost:4242");
 
-        socket.onopen = () => {
-            console.log("Connection opened");
-        }
-
-        socket.onmessage = (event: { data: any; }) => {
-            console.log("Received:", event.data);
-            document.getElementById("test2").innerHTML = event.data;
-        }
-
-        socket.onclose = () => {
-            console.log("Connection closed");
-            setTimeout(connect, 5000);
-        }
-
-        socket.onerror = (error: any) => {
-            console.error("WebSocket error:", error);
-            socket.close(); // ensure it triggers onclose and retries
-        };
+    socket.onopen = () => {
+        console.log("Connection opened");
     }
 
+    socket.onmessage = (event: { data: any; }) => {
+        console.log("Received:", event.data);
+        document.getElementById("test2").innerHTML = event.data;
+    }
 
-    connect();
+    socket.onclose = () => {
+        console.log("Connection closed");
+        setTimeout(connect, 5000);
+    }
+
+    socket.onerror = (error: any) => {
+        console.error("WebSocket error:", error);
+        socket.close(); // ensure it triggers onclose and retries
+    };
 }
+
+
+connect();
 // socket.addEventListener("message", (event) => {
 //     console.log("Received:", event.data);
 // });
