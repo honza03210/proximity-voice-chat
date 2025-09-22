a8939b5c4b5c092d3875eba366c8e1966faddc69
Jan Altrichter <xaltrich@fi.muni.cz>
2025-09-22 13:29:21 +0200

    onload

--- WebsocketClient.js
+++ WebsocketClient.js
@@ -1,23 +1,26 @@
-var urlParams = new URLSearchParams(window.location.search);
-document.getElementById("test").innerHTML = urlParams.get("username") + " is trying to connect to room associated with server " + urlParams.get("server_id");
-function connect() {
-    var socket = new WebSocket("ws://localhost:4242");
-    socket.onopen = function () {
-        console.log("Connection opened");
-    };
-    socket.onmessage = function (event) {
-        console.log("Received:", event.data);
-    };
-    socket.onclose = function () {
-        console.log("Connection closed");
-        setTimeout(connect, 5000);
-    };
-    socket.onerror = function (error) {
-        console.error("WebSocket error:", error);
-        socket.close(); // ensure it triggers onclose and retries
-    };
+function startup() {
+    var urlParams = new URLSearchParams(window.location.search);
+    document.getElementById("test1").innerHTML = urlParams.get("username") + " is trying to connect to room associated with server " + urlParams.get("server_id");
+    function connect() {
+        var socket = new WebSocket("ws://localhost:4242");
+        socket.onopen = function () {
+            console.log("Connection opened");
+        };
+        socket.onmessage = function (event) {
+            console.log("Received:", event.data);
+            document.getElementById("test2").innerHTML = event.data;
+        };
+        socket.onclose = function () {
+            console.log("Connection closed");
+            setTimeout(connect, 5000);
+        };
+        socket.onerror = function (error) {
+            console.error("WebSocket error:", error);
+            socket.close(); // ensure it triggers onclose and retries
+        };
+    }
+    connect();
 }
-connect();
 // socket.addEventListener("message", (event) => {
 //     console.log("Received:", event.data);
 // });
--- WebsocketClient.ts
+++ WebsocketClient.ts
@@ -1,33 +1,35 @@
 
-let urlParams = new URLSearchParams(window.location.search);
+function startup() {
+    let urlParams = new URLSearchParams(window.location.search);
 
-document.getElementById("test1").innerHTML = urlParams.get("username") + " is trying to connect to room associated with server " + urlParams.get("server_id");
+    document.getElementById("test1").innerHTML = urlParams.get("username") + " is trying to connect to room associated with server " + urlParams.get("server_id");
 
-function connect(){
-    let socket = new WebSocket("ws://localhost:4242");
+    function connect() {
+        let socket = new WebSocket("ws://localhost:4242");
 
-    socket.onopen = () => {
-        console.log("Connection opened");
+        socket.onopen = () => {
+            console.log("Connection opened");
+        }
+
+        socket.onmessage = (event: { data: any; }) => {
+            console.log("Received:", event.data);
+            document.getElementById("test2").innerHTML = event.data;
+        }
+
+        socket.onclose = () => {
+            console.log("Connection closed");
+            setTimeout(connect, 5000);
+        }
+
+        socket.onerror = (error: any) => {
+            console.error("WebSocket error:", error);
+            socket.close(); // ensure it triggers onclose and retries
+        };
     }
 
-    socket.onmessage = (event: { data: any; }) => {
-        console.log("Received:", event.data);
-        document.getElementById("test2").innerHTML = event.data;
-    }
 
-    socket.onclose = () => {
-        console.log("Connection closed");
-        setTimeout(connect, 5000);
-    }
-
-    socket.onerror = (error: any) => {
-        console.error("WebSocket error:", error);
-        socket.close(); // ensure it triggers onclose and retries
-    };
+    connect();
 }
-
-
-connect();
 // socket.addEventListener("message", (event) => {
 //     console.log("Received:", event.data);
 // });
